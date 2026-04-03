52f2cdc1182047f364257d1a56c186488fc460a0
Elistar_DJ <elistardj@outlook.com>
2026-04-03 19:35:25 +0300

    add sentry handler

---
+++ src/sentry.instruments.js
@@ -0,0 +1,10 @@
+const Sentry = require("@sentry/node");
+
+Sentry.init({
+  dsn: "https://e3399412d805639978a21352ae914d30@o4511157006237696.ingest.de.sentry.io/4511157008334928",
+  integrations: [
+    Sentry.consoleLoggingIntegration({ levels: ["log", "warn", "error"] }),
+  ],
+  enableLogs: true,
+  sendDefaultPii: true,
+});
